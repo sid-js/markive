686bf50d2e61062fcf082edd7fed82d872adfad8
Sidharth Sreejil <sidhsreejil@gmail.com>
2023-10-06 23:39:29 +0530

    setup: main.go file and chi router

---
+++ src/api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+	http.ListenAndServe(":3000", r)
+}
