5d3752e43d4662307eb95bae9eccd45ab22a9594
RISHIKESHk07 <117551967+RISHIKESHk07@users.noreply.github.com>
2023-10-09 14:28:33 +0530

    Updated models.go with primary keys
    - add primary keys for the models

--- src/api/models/models.go
+++ src/api/models/models.go
@@ -7,17 +7,21 @@ import (
 
 type Author struct {
     gorm.Model
+    AuthorID uint64 `gorm:"primaryKey"`
     Name     string `json:"name"`
     Bio      string `json:"bio"`
     Username string `json:"username" gorm:"unique"`
+    Blogs    []Blog `json:"blogs"`
 }
 
 type Blog struct {
     gorm.Model
+    BlogID    uint64    `gorm:"primaryKey"`
     Title     string    `json:"title"`
     Content   string    `json:"content"`
     CreatedAt time.Time `json:"created_at"`
     UpdatedAt time.Time `json:"updated_at"`
+    AuthorID  uint      `json:"-"`
     Author    Author    `json:"author"`
 }
 
